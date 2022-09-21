8fd279e890ff9065f2b9e9dc92c1704268e7a1d3
kristylyashenko <kristinalyashenko25@gmail.com>
2022-09-21 19:55:46 +0300

    add function

--- task_004.py
+++ task_004.py
@@ -3,6 +3,7 @@ import math
 x = int(input("Введіть значення x: "))
 y = int(input("Введіть значення y: "))
 
-f = ((x ** 3 + y ** 3) ** 1/4) + math.sin(3*x + 1) + math.log1p(math.fabs(x)) - math.e ** y-x
+def func(x, y):
+    return ((x ** 3 + y ** 3) ** 1/4) + math.sin(3*x + 1) + math.log1p(math.fabs(x)) - math.e ** y-x
 
-print("Результат значення f: ", f)
+print("Результат значення f: ", func(x, y))
